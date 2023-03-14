4470eef94bfdb76b1cbe3725c8dd56a3def9c7e6
Gabriel Aurélio <125391023+gabzcode777@users.noreply.github.com>
2023-03-14 14:13:55 -0300

    Update engine.js

--- www/components/engine.js
+++ www/components/engine.js
@@ -9,7 +9,32 @@ function olamundo() {
     altura = document.getElementById ('altura').value
    
     imc = peso /  ( altura * altura ) 
+    
     document.getElementById("IMC").textContent = imc
     alert(imc)
 
+    if(imc <= 18.5) {
+
+       document.getElementById('IMC'). textContent = imc + "Você está abaixo do peso!"
+    }
+    
+    else if((imc > 18.5) && (imc <= 24.9)) {
+     
+     document.getElementById('IMC').textContent = imc + "Você está no peso ideal!"
+
    }
+
+   else if((imc > 25) && (imc <= 29.9)) {
+     
+     document.getElementById('IMC').textContent = imc + "Você está no sobrepeso!"
+
+   }
+
+
+   else if((imc > 30)) {
+     
+     document.getElementById('IMC').textContent = imc + "Você está na obesidade"
+
+   }
+
+} 
